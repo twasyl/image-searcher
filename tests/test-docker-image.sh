19189fdca52d885fba6ff123f00b8ce7021fb11f
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2024-06-21 09:51:52 +0200

    Testing using a script

---
+++ tests/test-docker-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run the docker image and ensure it finds offensive words
+OUT=$(docker run --rm -v ./tests/resources:/work twasyl/image-searcher:latest)
+if [[ $OUT != *master* ]]; then
+    echo "No offensive words have been found while expected!"
+    exit 1
+fi
