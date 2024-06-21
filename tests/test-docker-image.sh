dc3b40ed68211031621a26ed533413f503b00542
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2024-06-21 10:01:29 +0200

    Trying to run tests

--- tests/test-docker-image.sh
+++ tests/test-docker-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the docker image and ensure it finds offensive words
-OUT=$(docker run --rm -v ./tests/resources:/work twasyl/image-searcher:latest)
+OUT=$(docker run --rm -v ./resources:/work twasyl/image-searcher:latest)
 if [[ $OUT != *master* ]]; then
     echo "No offensive words have been found while expected!"
     exit 1
